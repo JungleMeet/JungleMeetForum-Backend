87888feacd2eab8d635dfe4e573a81e6890d4ede
devharris7 <harris.wang@mail.com>
2022-09-18 14:59:24 +1000

    fix: bug

--- utils/swagger.js
+++ utils/swagger.js
@@ -4,13 +4,13 @@ module.exports = swaggerJsDoc({
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Virtual Queuing Apis Documentation',
+      title: 'Jungle Meet Apis Documentation',
       version: '1.0.0',
       contact: {
         name: 'Roy',
         email: 'wanyansuroy@gmail.com',
       },
-      description: 'This is the API documentation for Virtual Queuing',
+      description: 'This is the API documentation for JungleMeet Queuing',
     },
   },
   apis: ['controllers/*.js'],
