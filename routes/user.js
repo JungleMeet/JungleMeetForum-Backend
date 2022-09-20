c284946d9398e418632e5d35918ba2539904c07f
Rachel Fang <yanhanfan828@gmail.com>
2022-09-20 18:18:43 +1000

    feat: TJM-1 add create user

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const { getUserById, createUser } = require('../controllers/user');
 const userRouter = Router();
 
 userRouter.get('/:id', getUserById);
+
 userRouter.post('/', createUser);
 
 module.exports = userRouter;
