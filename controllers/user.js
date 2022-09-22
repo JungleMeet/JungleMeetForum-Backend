3718bbed0fd45d0fe767127f7b6695605b562d2b
MSI\Kim <108210462+Alvolutionor@users.noreply.github.com>
2022-09-22 11:23:49 +1000

    feat: reset password

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,6 @@ const User = require('../models/User');
 
 const getUserById = async (req, res) => {
   const { id } = req.params;
-  
 
   try {
     const user = await User.findById(id);
@@ -17,12 +16,10 @@ const getUserById = async (req, res) => {
 const createUser = async (req, res) => {
   const { name, password, email, avatar, bgImg } = req.body;
 
-
   try {
     const now = new Date();
-    const user = new User({name, password, email, avatar, bgImg, createTime: now});
+    const user = new User({ name, password, email, avatar, bgImg, createTime: now });
     const ret = await user.save();
-    
 
     return res.status(StatusCodes.OK).json(ret);
   } catch (err) {
@@ -30,6 +27,23 @@ const createUser = async (req, res) => {
   }
 };
 
+const resetPassword = async (req, res) => {
+  const { id, newPassword, oldPassword } = req.body;
+
+  try {
+    const user = await User.findById(id);
+    if (user.password === oldPassword) {
+      console.log('cor');
+      user.update({ $set: { password: newPassword } }, { runValidators: true }).save();
+    }
+    return res.status(StatusCodes.OK).json(user);
+  } catch (err) {
+    return res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
 module.exports = {
-  getUserById, createUser,
+  getUserById,
+  createUser,
+  resetPassword,
 };
