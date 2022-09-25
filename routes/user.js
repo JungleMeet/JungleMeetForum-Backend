6bfadbc7f6498687aaf264375ac4f12e9cf883ee
Alvolutionor <108210462+Alvolutionor@users.noreply.github.com>
2022-09-25 13:23:53 +1000

    feat: reset password (#6)
    Co-authored-by: Rachel-Fang <85208717+Rachel-Fang@users.noreply.github.com>

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { getUserById, createUser } = require('../controllers/user');
+const { getUserById, createUser, resetPassword } = require('../controllers/user');
 
 const userRouter = Router();
 
 userRouter.get('/:id', getUserById);
 
 userRouter.post('/', createUser);
+userRouter.post('/reset/', resetPassword);
 
 module.exports = userRouter;
