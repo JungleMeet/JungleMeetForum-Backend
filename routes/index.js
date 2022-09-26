72099f038033981a39c958c9c636ca709f29bfda
yestinlin <1002902457@qq.com>
2022-09-26 11:55:14 +1000

    userPost-Create

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRouter = require('./user');
+const postRouter = require('./post');
 
 const v1Router = express.Router();
 
 v1Router.use('/users', userRouter);
+v1Router.use('/posts', postRouter);
 
 module.exports = v1Router;
