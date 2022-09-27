d06c22dfae6305fd5ac403b14d7fb13b679f6789
Ting Huang <tinghuang.dev@gmail.com>
2022-09-28 08:24:07 +1000

    feat: create put method to updage post by id and get method to get all the posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,10 +1,9 @@
 const { StatusCodes } = require('http-status-codes');
 const Post = require('../models/Post');
 
-
 const createPost = async (req, res) => {
   const { title, author, content, hashtag, bgImg } = req.body;
-  
+
   try {
     const now = new Date();
     const post = new Post({ title, author, content, hashtag, bgImg, createTime: now });
@@ -16,6 +15,36 @@ const createPost = async (req, res) => {
   }
 };
 
+const getAllPosts = async (req, res) => {
+  try {
+    const allPosts = await Post.find();
+
+    return res.status(StatusCodes.OK).json(allPosts);
+  } catch (err) {
+    return res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+
+  try {
+    await Post.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: post,
+      },
+      { runValidator: true, useFindAndModify: true, new: true }
+    );
+    return res.status(StatusCodes.OK).json(post);
+  } catch (err) {
+    return res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
 module.exports = {
-  createPost
+  createPost,
+  updatePost,
+  getAllPosts,
 };
--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-const { createPost } = require('../controllers/post');
+const auth = require('../middleware/auth');
+const { createPost, updatePost, getAllPosts } = require('../controllers/post');
 
 const postRouter = Router();
 
 postRouter.post('/', createPost);
+postRouter.get('/', getAllPosts);
 
+postRouter.put('/:id', auth, updatePost);
 
 module.exports = postRouter;
