a74037111e1fd938ce9ee16b9736bab2673d40a0
tinghuang <101805508+tinghuang-dev@users.noreply.github.com>
2022-09-28 22:59:03 +1000

    Tjm 20 delete comment set visible false get all comment (#19)
    * feat: create a put method to change the visible to false in comment and add getAllComments method
    
    * feat: bring back getCommentById feature
    
    * fix: typo

--- controllers/comment.js
+++ controllers/comment.js
@@ -13,6 +13,16 @@ const createComment = async (req, res) => {
   }
 };
 
+const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find();
+
+    return res.status(StatusCodes.OK).json(comments);
+  } catch (err) {
+    return res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
 const getCommentById = async (req, res) => {
   const { id } = req.params;
 
@@ -25,7 +35,25 @@ const getCommentById = async (req, res) => {
   }
 };
 
+const deleteCommentById = async (req, res) => {
+  const { id } = req.params;
+  const { visible } = req.body;
+
+  try {
+    await Comment.findByIdAndUpdate({ _id: id }, [{ $set: { visible: { $toBool: visible } } }], {
+      runValidator: true,
+      useFindAndModify: true,
+      new: true,
+    });
+    return res.status(StatusCodes.OK).json('Your comment has already been deleted!');
+  } catch (err) {
+    return res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
 module.exports = {
   createComment,
+  getAllComments,
   getCommentById,
+  deleteCommentById,
 };
