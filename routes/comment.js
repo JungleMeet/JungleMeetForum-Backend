fe5a59601427f14c672244c84560394c917e3f70
Tom <95953466+TomZhong-au@users.noreply.github.com>
2022-10-02 14:21:04 +1100

    feat: add admin guard (#30)

--- routes/comment.js
+++ routes/comment.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const auth = require('../middleware/auth');
+const adminGuard = require('../middleware/adminGuard');
+
 const {
   createComment,
   getAllComments,
@@ -20,6 +22,7 @@ commentRouter.use(auth);
 commentRouter.post('/', createComment);
 commentRouter.put('/:id', updateComment);
 // Wd don't delete comment but update "visible" value to false and then the comment won't show anymore.
-commentRouter.put('/delete/:id', deleteCommentById);
+// only admin can delete a comment
+commentRouter.put('/delete/:id', adminGuard, deleteCommentById);
 
 module.exports = commentRouter;
