f6891f7242ad6e4c81b990f8c90ced3b0f4343fc
JustinLin <36983969+yestinlin@users.noreply.github.com>
2022-10-04 21:38:31 +1100

    userpost-swaggerui (#36)

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,103 @@
 const { StatusCodes } = require('http-status-codes');
 const Post = require('../models/Post');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - title
+ *         - content
+ *         - bgImg
+ *         - author
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The Auto-generated id of a post
+ *         title:
+ *           type: string
+ *           description: title of the post
+ *         content:
+ *           type: string
+ *           description: content of the post
+ *         bgImg:
+ *           type: string
+ *           description: background image of the the post
+ *         hashtag:
+ *           type: string
+ *           description: tag other user
+ *         author:
+ *           type: string
+ *           description: author of the post
+ *         createdTime:
+ *           type: string
+ *           description: the time of creating the post
+ *         updatedTime:
+ *           type: string
+ *           description: the time of updating the post
+ *         visible:
+ *           type: boolean
+ *           description: status of deleting the post
+ *         resourceId:
+ *           type: string
+ *           description: resource id of the post
+ *         viewCount:
+ *           type: number
+ *           description: the number of the post views
+ *         postType:
+ *           type: string
+ *           description: the type of the post
+ *         follower:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: follower of the user
+ *         like:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: the number of post like
+ *       example:
+ *         _id: 63338ae4d11e553b55138c9e
+ *         title: abd
+ *         content: 1234
+ *         createdTime: 2022-10-03T01:33:24.275Z
+ *         bgImg: xxxxx
+ *         visible: true
+ *         updatedTime: 2022-09-30T06:01:26.183Z
+ *         viewCount: 111
+ *         postType: userPost
+ *         like: []
+ *         author: 6332d81195b55eda2a612059
+ *         follower: [6332d81195b55eda2a612058, 6332d81195b55eda2a612057]
+ * paths:
+ *   /posts:
+ *     post:
+ *       tags:
+ *         - post
+ *       summary: Add a new post
+ *       description: Return created post
+ *       operationId: createPost
+ *       requestBody:
+ *         description:
+ *           Add a new post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post{title, content, author, bgImg}'
+ *       responses:
+ *         '200':
+ *           description: successful operation
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Post'
+ *         '404':
+ *           description: Not found
+ */
+
 const createPost = async (req, res) => {
   const { title, author, content, hashtag, bgImg } = req.body;
 
