57fee97ca837bd8eebb0ec017dc1165fd982a774
Rachel Fang <yanhanfan828@gmail.com>
2022-10-06 14:44:15 +1100

    fix: create user swagger

--- controllers/post.js
+++ controllers/post.js
@@ -16,62 +16,64 @@ const Post = require('../models/Post');
  *         _id:
  *           type: string
  *           description: The Auto-generated id of a post
+ *           example: 63338ae4d11e553b55138c9e
  *         title:
  *           type: string
  *           description: title of the post
+ *           example: abd
  *         content:
  *           type: string
  *           description: content of the post
+ *           example: 1234
  *         bgImg:
  *           type: string
  *           description: background image of the the post
+ *           example: xxxx
  *         hashtag:
  *           type: string
- *           description: tag other user
+ *           description: tag post
+ *           example: horror
  *         author:
  *           type: string
  *           description: author of the post
+ *           example: 6332d81195b55eda2a612059
  *         createdTime:
  *           type: string
  *           description: the time of creating the post
+ *           example: 2022-10-03T01:33:24.275Z
  *         updatedTime:
  *           type: string
  *           description: the time of updating the post
+ *           example: 2022-09-30T06:01:26.183Z
  *         visible:
  *           type: boolean
  *           description: status of deleting the post
+ *           example: true
  *         resourceId:
  *           type: string
  *           description: resource id of the post
+ *           example: xxxxxx
  *         viewCount:
  *           type: number
  *           description: the number of the post views
+ *           example: 11
  *         postType:
  *           type: string
  *           description: the type of the post
+ *           example: usePost
  *         follower:
  *           type: array
  *           items:
  *             type: string
  *           description: follower of the user
+ *           example: [6332d81195b55eda2a612058, 6332d81195b55eda2a612057]
  *         like:
  *           type: array
  *           items:
  *             type: string
- *           description: the number of post like
- *       example:
- *         _id: 63338ae4d11e553b55138c9e
- *         title: abd
- *         content: 1234
- *         createdTime: 2022-10-03T01:33:24.275Z
- *         bgImg: xxxx
- *         visible: true
- *         updatedTime: 2022-09-30T06:01:26.183Z
- *         viewCount: 111
- *         postType: userPost
- *         like: []
- *         author: 6332d81195b55eda2a612059
- *         follower: [6332d81195b55eda2a612058, 6332d81195b55eda2a612057]
+ *           description: the userId of users who like post
+ *           example: [6332d81195b55eda2a612058, 6332d81195b55eda2a612057]
+ * 
  * paths:
  *   /posts:
  *     post:
