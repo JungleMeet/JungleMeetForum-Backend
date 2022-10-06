b24884629f07eb0413c3470b4dc5cbc012bb0802
Tom <tomzhongcn@gmail.com>
2022-10-06 18:08:30 +1100

    feat: add timestamp in schema

--- models/Notification.js
+++ models/Notification.js
@@ -1,22 +1,32 @@
 const mongoose = require('mongoose');
 
-const NotificationSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
+const NotificationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+    triggerUserId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+    targetPostId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Post',
+    },
+    targetCommentId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Comment',
+    },
+    action: {
+      type: String,
+      enum: ['replied', 'published', 'mentioned', 'liked', 'followed', 'commented'],
+    },
+    viewed: {
+      type: Boolean,
+    },
   },
-  triggerUserId: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-  },
-  targetPostId: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Post',
-  },
-  targetCommentId: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Comment',
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Notification', NotificationSchema);
