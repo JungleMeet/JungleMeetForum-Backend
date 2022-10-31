84fb44ae7b77b78b946f9d0104bdd59db03a2606
Rachel Fang <yanhanfan828@gmail.com>
2022-11-01 04:32:40 +1100

    fix: wrap loginmsg

--- controllers/user.js
+++ controllers/user.js
@@ -157,7 +157,11 @@ const userLogIn = async (req, res) => {
         algorithm: 'HS256',
         expiresIn: process.env.JWT_EXPIRE_TIME,
       });
-      return res.status(StatusCodes.OK).json({ token, userName: user.name, userRole: user.role });
+      const user_info = {
+        userName: user.name,
+        userRole: user.role,
+      };
+      return res.status(StatusCodes.OK).json({ token, user_info });
     }
     return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Wrong password, try again' });
   } catch (err) {
