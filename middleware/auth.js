870dee768b800419185b08494190b7055dcab173
Rachel Fang <yanhanfan828@gmail.com>
2022-11-01 16:59:10 +1100

    fix: auth token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,17 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-const auth = (req, res, next) =>
-  // const { token } = req.cookies;
-
-  // if (!token) return res.status(401).json({ message: 'no token is presented' });
-  // // verify() is a fake function, replace it with a real jwt function
-  // req.userId = jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
-  //   if (err) {
-  //     return res.status(401).json({ message: 'invalid token' });
-  //   }
-  //   return payload.userId;
-  // });
-  next();
+const auth = (req, res, next) => {
+  const bearerHeader = req.headers.authorization;
+  const bearer = bearerHeader.split(' ');
+  const token = bearer[1];
+  if (!token) return res.status(401).json({ message: 'no token is presented' });
+  // verify() is a fake function, replace it with a real jwt function
+  req.userId = jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+    if (err) {
+      return res.status(401).json({ message: 'invalid token' });
+    }
+    return payload.userId;
+  });
+  return next();
+};
 module.exports = auth;
